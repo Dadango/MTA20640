a3156e35f11ac633ba03735044a6f77ef4675c85
Christian Bodskov <cbodsk17@student.aau.dk>
2020-04-03 14:27:10 +0200

    Added tilemap, dragndrop, car

---
+++ Assets/Scripts/DragNDrop.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragNDrop : MonoBehaviour
+{
+    public float speed;
+    public float leeway = 0.05f;
+
+    private bool following;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        following = false;
+
+        leeway += 10;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && ((Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position).magnitude <= leeway))
+        {
+            following = !following;
+        }
+        if (following)
+        {
+            transform.position = Vector2.Lerp(transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition), speed);
+        }
+      }
+}
