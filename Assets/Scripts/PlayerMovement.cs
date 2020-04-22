41f4e81d70e1d77be249dc3d4970b71fab00c8c4
AntonioTonchev <36622262+AntonioTonchev@users.noreply.github.com>
2020-04-22 15:13:43 +0200

    Added "MoveBackward" or "left"

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,16 +9,16 @@ public class PlayerMovement : MonoBehaviour
     Vector3 moveForward = new Vector3(1.0f, 0.0f);
     Vector3 moveLeft = new Vector3(0.0f, 1.0f);
     Vector3 moveRight = new Vector3(0.0f, -1.0f);
+    Vector3 moveBackward = new Vector3(-1.0f, 0.0f);
 
     Vector3 currentPos = new Vector3();
     Vector3 destinationForward = new Vector3();
     Vector3 destinationLeft = new Vector3();
     Vector3 destinationRight = new Vector3();
+    Vector3 destinationBackward = new Vector3();
 
     public float smoothTime;
     Vector3 velocity = Vector3.zero;
-
-
 
     IEnumerator Move()
     {
@@ -29,7 +29,6 @@ public class PlayerMovement : MonoBehaviour
             destinationForward = currentPos + moveForward;
             while (true)
             {
-                //Debug.Log("Total magnitude is " + Mathf.Abs(transform.position.magnitude - destinationForward.magnitude));
                 yield return new WaitForEndOfFrame();
                 transform.position = Vector3.SmoothDamp(transform.position, destinationForward, ref velocity, smoothTime);
                 if (transform.eulerAngles.z != 0)
@@ -42,7 +41,25 @@ public class PlayerMovement : MonoBehaviour
                     break;
                 }
             }
-
+        }
+        else if (Input.GetKeyDown("left"))
+        {
+            currentPos = transform.position;
+            destinationBackward = currentPos + moveBackward;
+            while (true)
+            {
+                yield return new WaitForEndOfFrame();
+                transform.position = Vector3.SmoothDamp(transform.position, destinationBackward, ref velocity, smoothTime);
+                if (transform.eulerAngles.z != 180.0f)
+                {
+                    transform.eulerAngles = new Vector3(0.0f, 0.0f, 180.0f);
+                }
+                if (Mathf.Abs(transform.position.magnitude - destinationBackward.magnitude) < 0.02f)
+                {
+                    transform.position = destinationBackward;
+                    break;
+                }
+            }
         }
         else if (Input.GetKeyDown("up"))
         {
@@ -110,6 +127,29 @@ public class PlayerMovement : MonoBehaviour
         yield return null;
     }
 
+    IEnumerator DriveBackward()
+    {
+        driving = true;
+        currentPos = transform.position;
+        destinationBackward = currentPos + moveBackward;
+        while (true)
+        {
+            yield return new WaitForEndOfFrame();
+            transform.position = Vector3.SmoothDamp(transform.position, destinationBackward, ref velocity, smoothTime);
+            if (transform.eulerAngles.z != 180.0f)
+            {
+                transform.eulerAngles = new Vector3(0.0f, 0.0f, 180.0f);
+            }
+            if (Mathf.Abs(transform.position.magnitude - destinationBackward.magnitude) < 0.02f)
+            {
+                transform.position = destinationBackward;
+                break;
+            }
+        }
+        driving = false;
+        yield return null;
+    }
+
     IEnumerator DriveUp() {
         driving = true;
         currentPos = transform.position;
@@ -158,7 +198,7 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
    void Update()
     {
-        if ((Input.GetKeyDown("down") || Input.GetKeyDown("up") || Input.GetKeyDown("right")) && driving == false)
+        if ((Input.GetKeyDown("down") || Input.GetKeyDown("up") || Input.GetKeyDown("right")) || Input.GetKeyDown("left") && driving == false)
         {
             StartCoroutine(Move());
         }
