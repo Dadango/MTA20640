fed3affa11e8b3f5553f8db9b7c90da66f8a1546
Christian Bodskov <cbodsk17@student.aau.dk>
2020-04-23 12:45:34 +0200

    Update DragNDrop.cs
    Now blocks follow cursor on spawn

--- Assets/Scripts/DragNDrop.cs
+++ Assets/Scripts/DragNDrop.cs
@@ -20,7 +20,7 @@ public class DragNDrop : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        following = false;
+        following = true;
         hoveredSlot = null;
 
         leeway += 10;
