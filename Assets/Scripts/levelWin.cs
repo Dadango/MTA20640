2cf73d38ee94756845e0425df41fc4786acdb516
Seyya <mrfm16@student.aau.dk>
2020-04-24 14:10:51 +0200

    New tags (trashcan and roadblock), added prefabs (roadblock and finishline), added new sprites

---
+++ Assets/Scripts/levelWin.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class levelWin : MonoBehaviour
+{
+    public string newLevelString;
+    //remember to build the scene in file -> build scene
+    private void OnTriggerEnter2D(Collider2D levelEnd)
+    {
+        //Application.LoadLevel("level 2")
+        if (levelEnd.gameObject.CompareTag("Player")) 
+        {
+            SceneManager.LoadScene(newLevelString);
+            Debug.Log("igothit");
+        }
+    }
+}
